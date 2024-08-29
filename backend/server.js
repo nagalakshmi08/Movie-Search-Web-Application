76384be2c2cd4827a6b0e7ca1d15cd704017d0fd
Naga Kanaka Lakshmi <121669832+nagalakshmi08@users.noreply.github.com>
2024-08-29 22:19:33 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -11,6 +11,10 @@ app.use(cors());
 app.use(express.json({ limit: '50mb', extended: true })); 
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
+app.get('/',(req,res)=>{
+  res.json('Hello');
+})
+
 // Routes
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/user', require('./routes/user'));
