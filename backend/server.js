d852dff0f98d935f0510585e8de2a05be319dea9
nagalakshmi08 <lakshmireddydwarampudi436@gmail.com>
2024-08-30 08:36:53 +0530

    server.js

--- backend/server.js
+++ backend/server.js
@@ -4,8 +4,10 @@ const cors = require('cors');
 
 const app = express();
 
+// Connect to the database
 connectDB();
 
+// Middleware
 app.use(cors());
 app.use(express.json({ limit: '50mb', extended: true })); 
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
@@ -14,7 +16,11 @@ app.use(express.urlencoded({ limit: '50mb', extended: true }));
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/user', require('./routes/user'));
 
+// Root Route
+app.get('/', (req, res) => {
+  res.send('Welcome to the Movie Search API');
+});
 
+// Start the server
 const PORT = process.env.PORT || 5000;
-
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
