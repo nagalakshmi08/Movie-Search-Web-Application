b69a961fcaa3b56b6c9812f95f07ff0969aacc66
Naga Kanaka Lakshmi <121669832+nagalakshmi08@users.noreply.github.com>
2024-08-30 07:48:46 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -4,27 +4,17 @@ const cors = require('cors');
 
 const app = express();
 
-// Connect to database
 connectDB();
 
-// Middleware setup
-app.use(cors({
-  origin: ["https://movie-search-web-application-frontend.vercel.app"],
-  methods: ["POST", "GET"],
-  credentials: true
-}));
-app.use(express.json({ limit: '50mb', extended: true }));
+app.use(cors());
+app.use(express.json({ limit: '50mb', extended: true })); 
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
-// Test route
-app.get('/', (req, res) => {
-  res.json('Hello');
-});
-
-// API Routes
+// Routes
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/user', require('./routes/user'));
 
-// Start the server
+
 const PORT = process.env.PORT || 5000;
+
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
